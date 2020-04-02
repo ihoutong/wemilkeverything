213cc45d795a125d38b63590c068c7498057ffc8
= <ihoutong93@gmail.com>
2020-04-02 19:35:37 -0400

    Add text for fun

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          The start of milking everything. Nothing is safe.
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
